6156550caa3fcb2b29681770aab26fd34cefe9a5
Riley Bruins <ribru17@hotmail.com>
2024-01-01 16:19:45 -0800

    chore(addreview): clean up comments and imports

--- screens/AddReview.tsx
+++ screens/AddReview.tsx
@@ -1,34 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { globalStyles } from '../styles/globalStyles'
-import {SafeAreaView, StyleSheet, TextInput, Button, Alert, Pressable} from 'react-native';
+import { Text, View } from 'react-native';
+import { globalStyles } from '../styles/globalStyles';
 
-
-// var Themeparks = require('themeparks');
-
-
-// var api = new Themeparks.DestinationsApi()
-// api.getDestinations().then(function(data: any) {
-//   console.log('API called successfully. Returned data: ' + data);
-// }, function(error: any) {
-//   console.error(error);
-// });
-
-
-
-
-const AddReview = ({ navigation }: { navigation: any }) => {
-  
+const AddReview = () => {
   return (
     <View style={globalStyles.container}>
       <Text>Add Review</Text>
-      {/* <Button
-            title="Register"
-            onPress={() => navigation.navigate('Register')}
-        /> */}
-            
     </View>
-  )
-}
+  );
+};
 
-export default AddReview
+export default AddReview;
